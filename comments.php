3df60e445d70871eca256ddc9935dcf5c32dca2f
ekosistema <46524536+ekosistema@users.noreply.github.com>
2020-12-13 10:51:33 +0100

    Update comments.php
    Update comments.php

--- comments.php
+++ comments.php
@@ -32,13 +32,13 @@ if ( post_password_required() ) {
 			if ( '1' === $fromscratch_comment_count ) {
 				printf(
 					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'fromscratch' ),
+					esc_html__( 'Un comentario en &ldquo;%1$s&rdquo;', 'fromscratch' ),
 					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
 				);
 			} else {
 				printf( 
 					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $fromscratch_comment_count, 'comments title', 'fromscratch' ) ),
+					esc_html( _nx( '%1$s comentarios en &ldquo;%2$s&rdquo;', '%1$s comentarios en &ldquo;%2$s&rdquo;', $fromscratch_comment_count, 'comments title', 'fromscratch' ) ),
 					number_format_i18n( $fromscratch_comment_count ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
 				);
@@ -65,7 +65,7 @@ if ( post_password_required() ) {
 		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if ( ! comments_open() ) :
 			?>
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'fromscratch' ); ?></p>
+			<p class="no-comments"><?php esc_html_e( 'Los comentarios estan cerrados.', 'fromscratch' ); ?></p>
 			<?php
 		endif;
 
