89d0444d142b9836bc9e6c22a533f54bf1768cf6
ekosistema <46524536+ekosistema@users.noreply.github.com>
2021-01-18 00:29:08 +0100

    Minor fixes
    Minor fixes

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -14,12 +14,7 @@
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php
-			fromscratch_posted_on();
-			fromscratch_posted_by();
-			?>
-		</div><!-- .entry-meta -->
+
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
---
+++ template-parts/search-form.php
@@ -0,0 +1,6 @@
+<form role="search" method="get" class="search-form2 py-3" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+		<input type="search" id="search-field2" class="search-field2 w-100 bg-light" placeholder="Buscar" value="" name="s" autocomplete="off">
+</form>
+
+
+<!-- Search form <//?php get_template_part('template-parts/search-form', 'search-form');?> -->
